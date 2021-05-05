c99180cde2f97e7d5f9cbd4455d96d180be1f804
Logan Saso <logansaso+tech@gmail.com>
2021-05-05 01:49:38 -0700

    Added one of the type conversions

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,30 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use std::ffi::{CString, CStr};
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub enum EffectSelector {
+    Denoiser,
+    Dereverb,
+    Dereverb_Denoiser,
+}
+
+impl Into<NvAFX_EffectSelector> for EffectSelector {
+    fn into(self) -> *const std::os::raw::c_char {
+        let cstr = CString::new(match self {
+            EffectSelector::Denoiser => {
+                "denoiser"
+            }
+            EffectSelector::Dereverb => {
+                "dereverb"
+            }
+            EffectSelector::Dereverb_Denoiser => {
+                "dereverb_denoiser"
+            }
+        }).expect("Constant conversion failed.");
+
+        unsafe { CStr::from_bytes_with_nul_unchecked(cstr.as_bytes_with_nul()).as_ptr() }
+    }
+}
